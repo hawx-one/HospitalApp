6fd303d3d626611047981f1440cf5d09ccaee98e
hawx-one <ivan.askold.shevtsiv@gmail.com>
2014-02-01 15:57:32 +0200

    Project Model Add

---
+++ HospitalMvcApplication.Model/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HospitalMvcApplication.Model
+{
+    interface IRepository
+    {
+        IQueryable<Role> Roles { get; }
+
+        bool CreateRole(Role instance);
+        bool UpdateRole(Role instance);
+        bool RemoveRole(int idRole);
+    }
+}
