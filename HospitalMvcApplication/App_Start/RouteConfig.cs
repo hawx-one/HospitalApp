99e77e532a285d8034d2bac82b5415224ddd0ba6
hawx-one <ivan.askold.shevtsiv@gmail.com>
2014-02-13 20:49:52 +0200

    WebApiConfig replaced with RouteConfig, few bug fix

--- HospitalMvcApplication/App_Start/RouteConfig.cs
+++ HospitalMvcApplication/App_Start/RouteConfig.cs
@@ -12,10 +12,12 @@ namespace HospitalMvcApplication
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
             routes.MapRoute(
-                name: "Default",
+                name: "DefaultApi",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { id = UrlParameter.Optional }
             );
         }
     }
--- HospitalMvcApplication/Areas/Default/Controllers/HomeController.cs
+++ HospitalMvcApplication/Areas/Default/Controllers/HomeController.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HospitalMvcApplication.Controllers;
 using HospitalMvcApplication.Model;
 
 namespace HospitalMvcApplication.Areas.Default.Controllers
 {
-    public class HomeController : DefaultController
+    public class HomeController : BaseController
     {
         //
         // GET: /Default/Home/
