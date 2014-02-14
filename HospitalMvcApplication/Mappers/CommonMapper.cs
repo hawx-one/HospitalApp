089f82578ce0ff615947ac5ae1ce09000262fd88
hawx-one <ivan.askold.shevtsiv@gmail.com>
2014-02-14 12:26:34 +0200

    fix

--- HospitalMvcApplication/Mappers/CommonMapper.cs
+++ HospitalMvcApplication/Mappers/CommonMapper.cs
@@ -12,13 +12,13 @@ namespace HospitalMvcApplication.Mappers
     {
         static CommonMapper()
         {
-            /*Mapper.CreateMap<User, UserView>()
+            Mapper.CreateMap<User, UserView>()
                     .ForMember(dest => dest.BirthdateDay, opt => opt.MapFrom(src => src.Birthdate.Day))
                     .ForMember(dest => dest.BirthdateMonth, opt => opt.MapFrom(src => src.Birthdate.Month))
                     .ForMember(dest => dest.BirthdateYear, opt => opt.MapFrom(src => src.Birthdate.Year));
             Mapper.CreateMap<UserView, User>()
                     .ForMember(dest => dest.Birthdate, opt => opt.MapFrom(src => new DateTime(src.BirthdateYear, src.BirthdateMonth, src.BirthdateDay)));
-            */
+            
         }
 
         public object Map(object source, Type sourceType, Type destinationType)
