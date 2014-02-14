573823ae72333ac4f809a2f44eb24056e1148fa7
Андрей <Epsulonnull@gmail.com>
2014-02-14 13:33:10 +0200

    Mapper

---
+++ HospitalMvcApplication/Areas/Default/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HospitalMvcApplication.Model;
+using Ninject;
+
+namespace HospitalMvcApplication.Areas.Default.Controllers
+{
+    public class HomeController : DefaultController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HospitalMvcApplication/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+using HospitalMvcApplication.Model;
+using HospitalMvcApplication.Mappers;
+
+namespace HospitalMvcApplication.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        [Inject]
+        public IRepository Repository { get; set; }
+   
+
+     [Inject]
+        public IMapper ModelMapper { get; set; }
+    }
+    
+}
